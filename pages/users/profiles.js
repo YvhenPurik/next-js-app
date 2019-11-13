fefcd8667b8ad34c9c76840133b3325fd0654160
Evgeniy Purik <evgeniy.purik@nearshorepartner.ch>
2019-11-13 11:53:50 +0200

    add path name

--- pages/users/profiles.js
+++ pages/users/profiles.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Nav from '../../components/nav'
+import axios from 'axios'
 
-const Home = () => (
-  <div>
+class Home extends React.Component  {
+  static async getInitialProps({pathname,query,asPth,req,res}){
+    let userData;
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    
+    try {
+      userData = response.data
+      //console.log('userData',userData)
+    } catch (error) {
+       console.error('error',error)
+    }
+    console.log('context',pathname)
+    return{
+         user:{
+           firasname:'jenya',
+           lastName:"Purik"
+         }
+    }
+    
+  }
+  constructor(props){
+    super(props)
+     
+  }
+  render(){
+  //  let res = this.props.userData.map((el)=>{
+  //     return el
+  //   })
+   //console.log('props',this.props)
+    return(
+      <div>
       <Nav/>
-    <h>Helo world users profale
-
-    </h>
+    <p>{"res"}</p>
+    <h>Helo world users profale 1231231</h>
   </div>
-)
+    )
+  }
+  
+}
 
 export default Home
