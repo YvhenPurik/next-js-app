7beb68ed6e9e1b16d292b619ad1138c1c242d7d4
YvhenPurik <purikevhen@gmail.com>
2020-02-01 19:05:46 +0200

    user del console.log

--- pages/users/profiles.js
+++ pages/users/profiles.js
@@ -1,52 +1,46 @@
-import React from 'react'
-import Nav from '../../components/nav'
-import axios from 'axios'
-import Link from 'react'
-class Home extends React.Component  {
-  static async getInitialProps({pathname,query,asPth,req,res}){
+import React from 'react';
+import Nav from '../../components/nav';
+import axios from 'axios';
+import Link from 'react';
+class Home extends React.Component {
+  static async getInitialProps({ pathname, query, asPth, req, res }) {
     let userData;
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
     try {
-      userData = response.data
+      userData = response.data;
       //console.log('userData',userData)
     } catch (error) {
-       console.error('error',error)
+      console.error('error', error);
     }
-    console.log('context',query)
-    return{
-      pathname:pathname,
-      query:query,
-     
-     
-         user:{
-           id:2,
-           firasname:'jenya',
-           lastName:"Purik"
-         }
-    }
-    
-  }
-  constructor(props){
-    super(props)
-     
-  }
-  render(){
-  //  let res = this.props.userData.map((el)=>{
-  //     return el
-  //   })
-   console.log('props',this.props.query)
-    return(
-      <div>
-        
-      <Nav userid={this.props.query.id}/>
-    <p>{"res"}</p>
+    return {
+      pathname: pathname,
+      query: query,
 
-    <h>Helo world users profale 1231231</h>
-  </div>
-    )
+      user: {
+        id: 2,
+        firasname: 'jenya',
+        lastName: 'Purik',
+      },
+    };
   }
-  
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    //  let res = this.props.userData.map((el)=>{
+    //     return el
+    //   })
+    console.log('props', this.props.query);
+    return (
+      <div>
+        <Nav userid={this.props.query.id} />
+        <p>{'res'}</p>
+
+        <h>Helo world users profale 1231231</h>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
